3d4b22fcd87602f9db956cc9184e60de2a27f2af
Jonathan Beaulieu <123.jonathan@gmail.com>
2017-04-25 09:55:42 -0500

    Updated test and debug scripts

--- debug.sh
+++ debug.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+usage()
+{
+cat <<TEXT
+debug.sh -- debug your myfs
+Usage: ./debug.sh [test_num]
+Ex: ./debug.sh 0
+TEXT
+
+exit 0
+}
+
+f=$(ls tests/$1*.test &> /dev/null)
+if [[ $? -ne 0 ]]; then
+    echo "Invalid test number"
+    echo
+    usage
+fi
+n=$(echo "$f" | wc -l)
+if [[ n -ne 1 ]]; then
+    usage
+fi
+
 echo "Cleaning..."
 make clean-all &> /dev/null
 if [[ $? -ne 0 ]]; then                  
@@ -22,8 +44,11 @@ fi
 test=$1
 
 echo "Will run test: $1"
-f=$(ls tests/$test*.test)
 name=$(echo $(basename $f) | cut -d"." -f1)
+
+echo "Name: '$name'"
+
+exit 0
 
 ./fly_swamp fs.iso < "$f" > "flies/$name"
 python read_fs.py --dump -p > "swamps/$name"
@@ -42,5 +67,4 @@ if [[ $? -eq 0 ]]; then
 else
     echo "Test '$name' Failed :("
 fi
-
 
--- test.sh
+++ test.sh
@@ -7,23 +7,96 @@
 # python <<< 's="Hello World"; print "Size:", len(s), "\nHex Data:", "".join(map(lambda x:hex(ord(x))[2:].zfill(2), s)).upper()'
 # Remember that every test should end with 'e'
 
+SOURCEFILE="myfs.c"
+DO_COMPILE=1
+DO_CLEANUP=1
+QUIT_ON_ERROR=0
+
+usage()
+{
+cat <<TEXT
+test.sh -- test your myfs
+Options:
+    -q  Quit on any error.
+    -d  Skip clean up on exit.
+    -h  Print this screen.
+TEXT
+
+exit 0
+}
+
+clean_all()
+{
+    make clean-all &> /dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "Error Cleaning! :("
+        echo "Run 'make clean-all' to see error."
+        exit 1
+    fi
+    rm -rf flies swamps
+}
+
+clean()
+{
+   make clean-all &> /dev/null
+	if [[ $? -ne 0 ]]; then
+    	echo "Error Cleaning! :("
+    	echo "Run 'make clean-all' to see error."
+    	exit 1
+	fi
+}
+
+while getopts "qdh" OPTION
+do
+    case $OPTION in
+        q)
+            QUIT_ON_ERROR=1
+            echo "Wlll quit on any error."
+            ;;
+        d)
+            DO_CLEANUP=0
+            echo "Will skip final cleanup."
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+
 echo "Pre-Cleaning..."
-make clean-all &> /dev/null
-if [[ $? -ne 0 ]]; then
-    echo "Error Cleaning! :("
-    echo "Run 'make clean' to see error."
-    exit 1
-fi
-rm -rf flies swamps
+clean_all
 mkdir flies swamps
 
-echo "Compiling..."
-make &> /dev/null #mount &> /dev/null
-if [[ $? -ne 0 ]]
+if [[ $DO_COMPILE == 1 ]]
 then
-    echo "Error compiling (run 'make' to see error)."
-    exit 1
+    echo "Compiling..."
+    make &> /dev/null #mount &> /dev/null
+    if [[ $? -ne 0 ]]
+    then
+        echo "Error compiling (run 'make' to see error)."
+        exit 1
+    fi
 fi
+
+BANNED="strcpy strcat strtok sprintf vsprintf gets strlen"
+
+echo "Checking for banned functions: $BANNED"
+for bfunc in $BANNED
+do
+    if grep -nE "\s${bfunc}\W" $SOURCEFILE &> /dev/null
+    then
+        echo "BANNED FUNCTION '$bfunc' DETECTED on line:"
+        echo
+        grep -nE "\s${bfunc}\W" $SOURCEFILE
+        echo
+        echo "Remove all banned functions from your code and try again."
+        exit 1
+    else
+        echo "    $bfunc"
+    fi
+done
+echo "All is good here."
+echo
 
 tests=0
 good=0
@@ -45,10 +118,16 @@ do
         diff "flies/$name" "tests/good/flies/$name" &> /dev/null
         if [[ $? -ne 0 ]]; then
             echo "Test '$name' Failed :("
+            if [[ $QUIT_ON_ERROR == 1 ]]; then
+                break
+            fi
         else
             diff "swamps/$name" "tests/good/swamps/$name" &> /dev/null
             if [[ $? -ne 0 ]]; then
                 echo "Test '$name' Failed :("
+                if [[ $QUIT_ON_ERROR == 1 ]]; then
+                    break
+                fi
             else
                 echo "Test '$name' Passed :)"
                 ((good++))
@@ -56,12 +135,23 @@ do
         fi
     else
         echo "Test '$name' Failed :("
+        if [[ $QUIT_ON_ERROR == 1 ]]; then
+            break
+        fi
     fi
     ((tests++))
 
 done
 
-#echo "Post-Cleaning..."
-make clean &> /dev/null
+if [[ $DO_CLEANUP == 1 ]]
+then
+    echo "Post-Cleaning..."
+    clean
+fi
 
-echo "Passed $good tests of $tests."
+echo
+echo "Passed $good tests of the $tests tested."
+echo
+echo "Thanks for using the Fly Swamp tester!"
+echo "Please come again soon."
+
